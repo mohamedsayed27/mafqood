0d730e6791845c26846063828214ffedb1fe9e67
Mohamed Sayed <100779216+mohamedsayed27@users.noreply.github.com>
2023-08-16 18:25:40 +0300

    Update reg_exp.dart

--- lib/core/constants/reg_exp.dart
+++ lib/core/constants/reg_exp.dart
@@ -1,5 +1,5 @@
 class RegularExp{
   static RegExp egyptPhoneRegex = RegExp(r'^01[0125][0-9]{8}$');
   static RegExp passwordRegex = RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{6,}$');
-
+  static const splitStringRegex = RegExp('r^\S(0|1)\-(\w+)');
 }
